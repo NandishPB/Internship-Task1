bebb7fd425c1cdfa8078939044fa932abaaf139c
Nandishpb <nandish.22ai026@sode-edu.in>
2024-08-05 17:30:07 +0530

    Initial commit: Set up basic server with form handling using Express and EJS

---
+++ server.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3001;
+
+app.set('view engine','ejs');  //set EJS as the templating view engine
+
+app.use(bodyParser.urlencoded({extended : true}));  //Use body-paraser to get the data which is filled in the form
+
+app.use(express.static('public'));
+
+app.get('/', (req,res) => {
+    res.render('index');
+});
+
+
+app.post('/sumbit', (req, res) => {
+    const{name,email,number,password} = req.body;
+    console.log(number)
+    // res.send(`<h1> Here is your response, Thank You</h1>
+    //     Name:${name}<br> Email: ${email} <br> Number: ${number}<br> Password:${password}`);
+    res.send(`<!DOCTYPE html>
+        <hmtl lang='en'>
+        <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
+        <title>Thank You</title>
+        <style>
+        body{
+        font-family:Arial,sans-serif;
+        background-color;#f4f4f9;
+        display:flex;
+        justify-content:center;
+        align-items:center;
+        height:100vh;
+        margin:0;
+        }
+        .container{
+        background-color:#fff;
+        border-radius:10px;
+        box-shadow:0 40px 8px rgba(0,0,0,0.1);
+        width:400px;
+        padding:20px;
+        text-algin:center;
+        }
+        h1{
+        color:#333;
+        margin-bottom:20px;
+        }
+        p{
+        color:#555;
+        margin-bottom: 20px;
+        }
+        a{
+        display:inline-block;
+        padding:10px, 20px;
+        background-color: #5cb85c;
+        color:white;
+        text-decoration: none;
+        bordrer-radius:5px;
+        transition:background-color 0.3s;
+        }
+        a:hover{
+        background-color: #4cae4c;
+        }
+        </style>
+        </head>
+        <body>
+        <div class="container">
+        <h1>Thank You</h1>
+        <p>You have submitted your form.</p>
+        <p>Name:${name}</p>
+        <p>Email:${email}</p>
+        <p>Number:${number}</p>
+        </div>
+        </body>
+        </html>       
+        `);
+});
+
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`);
+});
